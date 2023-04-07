2e80afdb9878bf01f682ae28615fb9b896bd3c76
aulianafahrian <aulianafahrian@gmail.com>
2023-04-07 16:42:56 +0700

    sss

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,6 +1,6 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel,isiTabelmahasiswa,isiTabeldosen } from "../temp/table.js";
+import { isiTabel,isiTabelmhs,isiTabeldsn } from "../temp/table.js";
 
 
 export function isiTable(results) {
