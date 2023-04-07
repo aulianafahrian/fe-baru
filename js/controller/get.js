2a38d8c23b94d73e4fb64e926fa3378fed252a37
aulianafahrian <aulianafahrian@gmail.com>
2023-04-07 09:24:44 +0700

    ss

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,9 +1,15 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "../temp/table.js";
+import { isiTabel, isiTabelp1 } from "../temp/table.js";
 export function isiTable(results) {
   results.forEach(isiRow);
 }
+
+export function isiTablep1(results) {
+  results.forEach(isiRowp1);
+}
+
+
 function isiRow(value) {
   let content = 
   isiTabel.replace("#NAMA#", value.biodata_mahasiswa.nama)
@@ -18,4 +24,18 @@ function isiRow(value) {
           .replace("#WARNA#", getRandomColor())
           .replace(/#WARNALOGO#/g, getRandomColorName());
   addInner("iniTabel", content);
+}
+
+function isiRowp1(value) {
+  let content = 
+  isiTabelp1.replace("#NAMA#", value.biodata_mahasiswa.nama)
+          .replace("#NPM#", value.biodata_mahasiswa.npm)
+          .replace("#JURUSAN#", value.biodata_mahasiswa.jurusan.nama)
+          .replace("#PRODI#", value.biodata_mahasiswa.prodi.nama)
+          .replace("#DOSENPEMBIMBING#", value.dosen_pembimbing.nama)
+          .replace("#DOSENPENGUJI#", value.dosen_penguji.nama)
+          .replace("#JUDUL#", value.judul)
+          .replace("#TANGGALSIDANG#", value.tanggal_sidang)
+
+  addInner("iniTabelp1", content);
 }
