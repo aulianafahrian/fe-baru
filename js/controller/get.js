e71546c5b7f11e4e66a69399e3241ad2b929642f
aulianafahrian <aulianafahrian@gmail.com>
2023-04-07 13:16:21 +0700

    sss

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,6 +1,6 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel, isiTabelp1 } from "../temp/table.js";
+import { isiTabel } from "../temp/table.js";
 
 
 export function isiTable(results) {
@@ -20,22 +20,3 @@ function isiRow(value) {
           .replace(/#WARNALOGO#/g, getRandomColorName());
   addInner("iniTabel", content);
 }
- 
-
-/* export function isiTablep1(results) {
-  results.forEach(isiRowp1);
-}
-function isiRowp1(value) {
-  let content = 
-  isiTabelp1.replace("#NAMA#", value.biodata_mahasiswa.nama)
-          .replace("#NPM#", value.biodata_mahasiswa.npm)
-          .replace("#JURUSAN#", value.biodata_mahasiswa.jurusan.nama)
-          .replace("#PRODI#", value.biodata_mahasiswa.prodi.nama)
-          .replace("#DOSENPEMBIMBING#", value.dosen_pembimbing.nama)
-          .replace("#DOSENPENGUJI#", value.dosen_penguji.nama)
-          .replace("#JUDUL#", value.judul)
-          .replace("#TANGGALSIDANG#", value.tanggal_sidang)
-          replace("#WARNA#", getRandomColor())
-          .replace(/#WARNALOGO#/g, getRandomColorName());
-  addInner("iniTabelp1", content);
-} */
--- js/fetch.js
+++ js/fetch.js
@@ -2,4 +2,3 @@ import { get } from "https://bukulapak.github.io/api/process.js";
 import { isiTable} from "./controller/get.js";
 import { urlAPI } from "./config/url.js";
 get(urlAPI, isiTable);
-//get(urlAPI, isiTablep1);
