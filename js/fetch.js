48977e2756e9a8ca8de4ca61f43423906e0a7980
aulianafahrian <aulianafahrian@gmail.com>
2023-04-07 13:11:09 +0700

    ss

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,5 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { isiTable, isiTablep1} from "./controller/get.js";
 import { urlAPI } from "./config/url.js";
-//get(urlAPI, isiTable);
-get(urlAPI, isiTablep1);
+get(urlAPI, isiTable);
+//get(urlAPI, isiTablep1);
